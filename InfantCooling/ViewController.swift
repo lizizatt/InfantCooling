b6e95402b00b3b28f1c7f82d62e78140e0625d21
Elizabeth Izatt <liz@berkeley.edu>
2018-11-25 15:02:57 -0800

    Initial Commit

---
+++ InfantCooling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfantCooling
+//
+//  Created by Elizabeth Izatt on 11/25/18.
+//  Copyright © 2018 LizIzatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
